59ba83e0497287cae5d1987d16249d703d4618a2
Walter Loburak <you@example.comvldlbrk@gmail.com>
2017-11-29 22:31:46 -0600

    API integration added, Render results from API, Finished.

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import './BusinessList.css';
 import Business from '../Business/Business';
+import './BusinessList.css';
 
 class BusinessList extends React.Component {
     render() {
         return (
             <div className="BusinessList">
-               { this.props.businesses.map(function(business) {
-                  return  <Business business={business} />
-                }) }   
+                {
+                    this.props.businesses.map(business => {
+                        return <Business key={business.id} business={business} />;
+                    })
+                }
             </div>
         );
     }
 }
 
 export default BusinessList;
-
-
